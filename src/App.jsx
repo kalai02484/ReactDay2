3291fa7a18ef01c1d89133ca1dd2b9d0b66fda6a
kalai02484 <kalai02484@gmail.com>
2025-11-15 18:44:56 +0530

    Basic Files Creations

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './assets/components/header'
+import Home from './assets/components/Home'
+import Footer from './assets/components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
